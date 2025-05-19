8a48bda54727adb67b8f50f3918721c654c3f7c8
Rion Hutsell <st.gish+gh@pm.me>
2025-05-19 18:25:00 -0400

    reenable debug

--- app.py
+++ app.py
@@ -318,4 +318,4 @@ if __name__ == '__main__':
         db.create_all()
     
     # Run application
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
